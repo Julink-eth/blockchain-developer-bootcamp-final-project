402348ffd60b41cbd14728aedd33d0d63f5eb873
Julien Fontanel <julien.fontanel12@gmail.com>
2021-11-18 17:46:19 -0600

    Updates

---
+++ precache-manifest.3bc8fc100406a36b8e11871ab5935225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751031d8aa51419604ec3cc610080d25",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8080377de3ad1e5f4b6e",
+    "url": "./static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "8080377de3ad1e5f4b6e",
+    "url": "./static/js/2.f5918001.chunk.js"
+  },
+  {
+    "revision": "98d949fda56eeca8a832",
+    "url": "./static/js/main.9a379a30.chunk.js"
+  },
+  {
+    "revision": "fec49678728315511c82",
+    "url": "./static/js/runtime-main.8d6e6c54.js"
+  },
+  {
+    "revision": "fd681d4afacedfa227aa57e2245b658f",
+    "url": "./static/media/metamask-logo.fd681d4a.svg"
+  }
+]);
