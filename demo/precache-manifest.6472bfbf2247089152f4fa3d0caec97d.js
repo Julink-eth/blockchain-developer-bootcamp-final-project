b77725f04ea6bdb95488bf312d45954b03a6f7f3
Julien Fontanel <julien.fontanel12@gmail.com>
2021-11-18 12:21:22 -0600

    Added demo page

---
+++ demo/precache-manifest.6472bfbf2247089152f4fa3d0caec97d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03aa07ac3f3ccc96365fa2c0d575f44d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7cbbbc61bd335a3de30",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "d7cbbbc61bd335a3de30",
+    "url": "/static/js/2.73eca231.chunk.js"
+  },
+  {
+    "revision": "93f07ccb15ec0927711d",
+    "url": "/static/js/main.48fb5ea0.chunk.js"
+  },
+  {
+    "revision": "92c89455b99cd109be4a",
+    "url": "/static/js/runtime-main.b5d1efea.js"
+  },
+  {
+    "revision": "fd681d4afacedfa227aa57e2245b658f",
+    "url": "/static/media/metamask-logo.fd681d4a.svg"
+  }
+]);
