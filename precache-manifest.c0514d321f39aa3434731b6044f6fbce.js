05a58eccf2ea4b4d21384c1296cc75f325c7a8b1
Julien Fontanel <julien.fontanel12@gmail.com>
2021-11-18 21:13:00 -0600

    Updates

--- precache-manifest.c0514d321f39aa3434731b6044f6fbce.js
+++ precache-manifest.c0514d321f39aa3434731b6044f6fbce.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f522403ae8b4e7f3e49ee32e30232263",
+    "revision": "4ff165d9034662342d30eed26d727556",
     "url": "/blockchain-developer-bootcamp-final-project/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/blockchain-developer-bootcamp-final-project/static/js/2.bcf77002.chunk.js"
   },
   {
-    "revision": "555038d87bfda62e4664",
-    "url": "/blockchain-developer-bootcamp-final-project/static/js/main.30ea5100.chunk.js"
+    "revision": "ded38cf898efb02455ad",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/main.1d13d42f.chunk.js"
   },
   {
     "revision": "b745693c2b46a4b93131",
