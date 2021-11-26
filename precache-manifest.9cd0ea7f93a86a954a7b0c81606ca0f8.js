05bcad3107fed413421c5d55bc148974fbdbd926
Julien Fontanel <julien.fontanel12@gmail.com>
2021-11-26 11:03:22 -0600

    Updates

---
+++ precache-manifest.9cd0ea7f93a86a954a7b0c81606ca0f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bee062711f75c0a9b7956bfc39391de",
+    "url": "/blockchain-developer-bootcamp-final-project/index.html"
+  },
+  {
+    "revision": "3aa2bf99d03682977bf4",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "3aa2bf99d03682977bf4",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/2.8df3162b.chunk.js"
+  },
+  {
+    "revision": "cc24cba1a2a79f5f420e",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/main.f260708f.chunk.js"
+  },
+  {
+    "revision": "b745693c2b46a4b93131",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/runtime-main.b93e3183.js"
+  },
+  {
+    "revision": "fd681d4afacedfa227aa57e2245b658f",
+    "url": "/blockchain-developer-bootcamp-final-project/static/media/metamask-logo.fd681d4a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blockchain-developer-bootcamp-final-project/precache-manifest.cd15160df7f3ad303251f1740836e5e5.js"
+  "/blockchain-developer-bootcamp-final-project/precache-manifest.9cd0ea7f93a86a954a7b0c81606ca0f8.js"
 );
 
 self.addEventListener('message', (event) => {
